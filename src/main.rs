6a0bd14abfbad1658fff5737fe155229e6d79900
Sam Schlinkert <sschlinkert@gmail.com>
2022-05-17 20:21:45 -0400

    prints a netflix list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Write;
+use std::path::PathBuf;
+
 fn main() {
+    let mut words_and_scores: Vec<(String, usize)> = vec![];
+    let raw_word_list = make_vec_from_filenames(&[PathBuf::from("raw.txt")]);
+    for word in raw_word_list {
+        words_and_scores.push((word.to_string(), number_of_clicks(word)));
+    }
+    words_and_scores.sort_by(|a, b| a.1.cmp(&b.1));
+    println!("{:?}", words_and_scores[2]);
+    println!("{:?}", words_and_scores[18000]);
+
+    let mut f = File::create("output.txt").expect("Unable to create file");
+    for word_and_score in words_and_scores {
+        writeln!(f, "{}", word_and_score.0).expect("Unable to write word to file");
+    }
+
     println!(
         "Distance between q and e is {}",
         measure_distance_between_chars('q', 'e')
@@ -64,4 +84,36 @@ fn measure_distance_of_word(word: String) -> usize {
         distance += measure_distance_between_chars(c[0], c[1]);
     }
     distance
+}
+
+fn number_of_clicks(word: String) -> usize {
+    word.chars().count() + measure_distance_of_word(word)
+}
+/// Takes a slice of `PathBuf`s representing the word list(s)
+/// that the user has inputted to the program. Then iterates
+/// through each file and addes each line to Vec<String>. (Blank
+/// lines and duplicate links will be handled elsewhere.)
+pub fn make_vec_from_filenames(filenames: &[PathBuf]) -> Vec<String> {
+    let mut word_list: Vec<String> = [].to_vec();
+    for filename in filenames {
+        let f = match File::open(filename) {
+            Ok(file) => file,
+            Err(e) => panic!("Error opening file {:?}: {}", filename, e),
+        };
+        let file = BufReader::new(&f);
+        for line in file.lines() {
+            let l = match line {
+                Ok(l) => l,
+                Err(e) => {
+                    eprintln!(
+                        "Error reading a line from file {:?}: {}\nWill continue reading file.",
+                        filename, e
+                    );
+                    continue;
+                }
+            };
+            word_list.push(l);
+        }
+    }
+    word_list
 }
